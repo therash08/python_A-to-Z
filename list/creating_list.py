9ca936f8cd5e89ec1829d80242ada8854bd608d9
therash45 <therash792@gmail.com>
2026-01-30 01:31:37 +0600

    Added list section code

---
+++ list/creating_list.py
@@ -0,0 +1,13 @@
+# print([])
+
+# #1d list
+# print([1,2,3,4,5])
+
+# #2d list
+# print([1,2,3,4,[5,6]])
+
+#3d list
+print([[[1,2],[3,4]],[[5,6],[7,8]]])
+print([1,True,5.6,5+6j,'Hello'])
+
+print(list("rasidul"))
---
+++ list/list.py
@@ -0,0 +1,13 @@
+# list = [10,'rash',67.43,[1,2,3]]
+# print(list)
+# print(type(list))
+
+# a = 6
+# print(id(6))
+
+l = [1,2,3]
+
+print(id(l))
+print(id(l[0]))     
+print(id(l[1]))     
+print(id(l[2]))      
