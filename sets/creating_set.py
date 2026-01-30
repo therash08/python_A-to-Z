ae26a29f8efc6a7722df8337980597b1181d3c14
therash45 <therash792@gmail.com>
2026-01-31 01:23:35 +0600

    Solved problems on sets

---
+++ sets/creating_set.py
@@ -0,0 +1,26 @@
+# s = set()
+# print(s)
+# print(type(s))
+
+# #1d set
+# s1 = {1,2,3,4}
+# print(s1)
+
+
+s3 = {1,2,'rash', 7.9, True}
+print(s3)
+
+s4 = set([1,2,3,4])
+print(s4)
+
+s5 = {1,1,2,3,3,5}
+print(s5)
+
+
+s1 = {1,2,3,4}
+s2 = {4,2,1,3}
+
+print(s1 == s2)
+
+
+
---
+++ sets/set_function.py
@@ -0,0 +1,55 @@
+# # len/sum/min/max/sorted
+# s = {3,1,4,5,2,7}
+# print(len(s))
+# print(sum(s))
+# print(max(s))
+# print(min(s))
+
+# union/update
+# s1 = {1,2,3,4,5}
+# s2 = {4,5,6,7,8}
+
+# # s1 | s2
+# s1.union(s1)
+
+# s1.update(s2)
+# print(s1)
+# print(s2)
+
+
+# intersection/intersection_update
+# s1 = {1,2,3,4,5}
+# s2 = {4,5,6,7,8}
+
+# s1.intersection(s2)
+
+# s1.intersection_update(s2)
+# print(s1)
+# print(s2)
+
+# difference/difference_update
+# s1 = {1,2,3,4,5}
+# s2 = {4,5,6,7,8}
+
+# s1.difference(s2)
+
+# s1.difference_update(s2)
+# print(s1)
+# print(s2)
+
+# symmetric_difference/symmetric_difference_update
+# s1 = {1,2,3,4,5}
+# s2 = {4,5,6,7,8}
+
+# s1.symmetric_difference(s2)
+
+# s1.symmetric_difference_update(s2)
+# print(s1)
+# print(s2)
+
+# isdisjoint/issubset/issuperset
+s1 = {1,2,3,4}
+s2 = {7,8,5,6}
+
+s1.isdisjoint(s2)
+print(s1)
